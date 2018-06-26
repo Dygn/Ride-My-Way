7dfead942e212ff1cb75f5a532be0b8726e22472
Dickens <40241345+Dygn@users.noreply.github.com>
2018-06-26 06:11:36 +0300

    update error handling

--- project-API/APP/API/routes.py
+++ project-API/APP/API/routes.py
@@ -1,13 +1,11 @@
-from APP.API import Bluep
-import json
-from flask import request, jsonify, make_response
-
 def ride_resource():
     """implement ride resource as a list of dictionary"""
     rides = [{"id":1, "destination":"Nairobi", "departure_point":"Eldoret", "fare":800, "driver":{"username":"Jesse", "gender":"male"}, "requests":[{"username":"jane","state":"accepted"},{"username":"jack","state":"accepted"},{"username":"frank", "state":"accepted"}],
  "passengers":{"usernames":["jane", "jack", "frank"],"number":3}, "stop_over":["Nakuru","Limuru"]}, {"id":2, "destination":"Mombasa", "departure_point":"Nairobi", "fare":1200, "driver":{"username":"Milac", "gender":"female"},
-"passengers":{"usernames":["jeff", "steve"],"number":2}, "stop_over":["Mtito Andei"]}] 
+"passengers":{"usernames":["jeff", "steve"],"number":2}, "stop_over":["Mtito Andei"], "requests":[{"username":"jeff","state":"accepted"},{"username":"steve","state":"accepted"}]}] 
     return rides
+
+
 
 @Bluep.route('/rides', methods=['GET'])
 def get_rides():
@@ -15,34 +13,42 @@ def get_rides():
     return make_response(jsonify(ride_resource()))
 
 @Bluep.route('/rides/<int:id>', methods=['GET'])
-def get_user(id):
-    """get ride by id"""
-    for ride in ride_resource():
-        if ride["id"] == id:
-            return make_response(jsonify(ride))
+def get_ride_by_id(id):
+    """access ride by id"""
+    if isinstance(id, int):
+        for ride in ride_resource():
+            if ride["id"] == id:
+                return make_response(jsonify(ride))
+        else:
+           return make_response(jsonify("message":"resource must have id 1 2 or one you just created"))
+    else:
+        return make_response(jsonify("message":"id must be an integer"))
+    
 
-@Bluep.route('/rides/<int:id>', methods=['PUT'])
-def update_ride_requests(id):
-    """update ride by id"""
-    for ride in ride_resource():
-       if ride["id"] == id:
-           data=request.get_json()
-           ride["requests"].append(data)
-           return make_response(jsonify(ride), 201)
+@Bluep.route('/rides/<int:id>', methods=['POST'])
+def post_ride_request(id):
+    """update ride by id""" 
+    if isinstance(id, int):
+        for ride in ride_resource():
+            if ride["id"] == id:
+                data = request.get_json()
+                ride["requests"].append(data)
+                return make_response(jsonify(ride), 201)
+        else:
+            return make_response(jsonify("message":"resource must have id 1 2 or one you just created"))
+    else:
+        return make_response(jsonify("message":"id must be an int"))
+
 
 @Bluep.route('/rides', methods=['POST'])
 def create_ride():
     """create ride"""
     data = request.get_json()
-    id = data['id']
-    destination = data['destination']
-    departure_point = data['departure_point']
-    fare = data['fare']
-    driver = data['driver']
-    return make_response(jsonify({"results":"success",
-                                 "status": "ok",
-                                 "id": id , 
-                                 "destination": destination, 
-                                 "fare": fare,
-                                 "driver":driver}), 201)
-
+    if isinstance(data["id"], int) and isinstance(data["departure"], string):
+        for ride in ride_resource():
+            if ride["id"] == id:
+                return make_response(jsonify("message":"ride id must be unique"))
+        else:
+            dt = ride_resource().append(data)
+            return make_response(jsonify(dt), 201)
+    
