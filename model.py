4159e25abfe1a6c74a980deb7b7f0861e56472de
Dickens <40241345+Dygn@users.noreply.github.com>
2018-06-29 13:09:57 +0300

    update model

--- model.py
+++ model.py
@@ -1,20 +1,54 @@
-from run import conn
+import psycopg2
+
+
 class UserModel():
     def __init__(self, username, password):
-        self.cur = None
-        self.result = None
-        self.username=username
-        self.password=password
-    #def create_usertable(self):
-        self.cur=conn.cursor()
-        #self.result = self.cur.execute("CREATE TABLE USERS(USERID INT PRIMARY KEY NOT NULL, USERNAME  VARCHAR(255) NOT NULL,PASSWORD  VARCHAR(255)  NOT NULL);")
+        self.username = username
+        self.password = password
+        try:
+            self.conn = psycopg2.connect(dbname="postgres", user="postgres", host="localhost", password="XABC", port=5432)
+        except:
+            print("connection error")
+        self.cur=self.conn.cursor()
+    
+    
     def save(self):
-        self.cur.execute('''INSERT INTO Users(username, password) VALUES(self.username, self.password)''')
-        
-        
-    def db_commit(self):
-        conn.commit()
+        self.cur.execute(""" INSERT INTO r_users (username, passwd) 
+                                VALUES (%s, %s); """
+                                , (self.username, self.password))
+        self.conn.commit()
 
+    def find_user(self, username):
+        self.cur.execute(""" SELECT username, passwd  FROM r_users WHERE username=username """)
+        print(self.cur.fetchone())
+        return self.cur.fetchone()
+        
+        
     def db_termination(self):
         self.cur.close()
-        conn.close()        
+        self.conn.close() 
+
+class RideModel():
+    def __init__(self, ridecreator, destination, departure, fare, d_date, request):
+        self.ridecreator = ridecreator
+        self.destination = destination
+        self.departure = departure
+        self.fare = fare
+        self.d_date = d_date
+        self.request = request
+
+        try:
+            self.conn = psycopg2.connect(dbname="postgres", user="postgres", host="localhost", password="XABC", port=5432)
+        except:
+            print("cant connect")
+            
+        self.cur=self.conn.cursor()
+
+    def save(self):
+        self.cur.execute(""" INSERT INTO ride_offers (ridecreator, destination, departure, fare, d_date, request)
+                            VALUES (%s, %s); """
+                            , (self.ridecreator, self.destination, self.departure, self.fare, self.d_date, self.request))
+        self.conn.commit()
+
+    
+        
